fd20a599c1af621a52a5fc11d93f398e28511f18
Michael Ferger <mferger92@gmail.com>
2016-05-06 11:46:37 -0600

    added attempt 2 folder with completed files

--- Attempt2/js/controllers/mainController.js
+++ Attempt2/js/controllers/mainController.js
@@ -1,8 +1,6 @@
 angular.module('app')
   .controller('MainController', function($state, MovieService){
       var vm = this;
-      vm.title = 'Welcome';
-
 
       vm.handleSearch = function(input){
         $state.go('home.list', {'search': input})
--- Attempt2/js/services/movieService.js
+++ Attempt2/js/services/movieService.js
@@ -1,10 +1,10 @@
 angular.module('app')
   .service('MovieService', function($http) {
     return {
-      getAllMovies: function(title) {
+      getAllMovies: function(title){
         return $http.get('https://www.omdbapi.com/?s=' + title + '&type=movie')
       },
-      getMovie: function(id) {
+      getMovie: function(id){
         return $http.get('https://www.omdbapi.com/?i=' + id + '&type=movie')
       }
     }
